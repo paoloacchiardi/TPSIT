8a4a6d3af40826926888e921abbd63304c71c653
Paolo Acchiardi <48619429+paoloacchiardi@users.noreply.github.com>
2020-03-04 15:15:42 +0100

    Add files via upload

---
+++ tree/es1/es1_tree.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct albero{
+    struct tree *left;
+    struct tree *right;
+    int key;
+    int value;
+}tree
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
